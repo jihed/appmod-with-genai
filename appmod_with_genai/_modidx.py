753065f92f2c733d8e2a565c067027f9297d8dbd
Jihed <jihed.mselmi@gmail.com>
2024-04-22 12:27:59 +0000

    fixing all issues

--- appmod_with_genai/_modidx.py
+++ appmod_with_genai/_modidx.py
@@ -5,4 +5,6 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://jihed.github.io',
                 'git_url': 'https://github.com/jihed/appmod-with-genai',
                 'lib_path': 'appmod_with_genai'},
-  'syms': {'appmod_with_genai.core': {'appmod_with_genai.core.foo': ('core.html#foo', 'appmod_with_genai/core.py')}}}
+  'syms': { 'appmod_with_genai.appmod_with_genai': { 'appmod_with_genai.appmod_with_genai.foo': ( 'core.html#foo',
+                                                                                                  'appmod_with_genai/appmod_with_genai.py')},
+            'appmod_with_genai.core': {'appmod_with_genai.core.foo': ('core.html#foo', 'appmod_with_genai/core.py')}}}
