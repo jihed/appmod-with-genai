8f341144c6a359463437f9c6b7bfbfff9a7a3b8a
Jihed <jihed.mselmi@gmail.com>
2024-04-22 12:38:01 +0000

    fixing nbddev

--- appmod_with_genai/_modidx.py
+++ appmod_with_genai/_modidx.py
@@ -5,6 +5,6 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://jihed.github.io',
                 'git_url': 'https://github.com/jihed/appmod-with-genai',
                 'lib_path': 'appmod_with_genai'},
-  'syms': { 'appmod_with_genai.appmod_with_genai': { 'appmod_with_genai.appmod_with_genai.foo': ( 'core.html#foo',
+  'syms': { 'appmod_with_genai.appmod_with_genai': { 'appmod_with_genai.appmod_with_genai.foo': ( 'appmod_with_genai_start.html#foo',
                                                                                                   'appmod_with_genai/appmod_with_genai.py')},
             'appmod_with_genai.core': {'appmod_with_genai.core.foo': ('core.html#foo', 'appmod_with_genai/core.py')}}}
